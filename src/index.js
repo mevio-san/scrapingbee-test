417d6078ec64e00fee3214096b47922fedeabb41
mevio-san <mevio@canvaslabs.co>
2025-07-02 19:06:59 +0200

    refactor: final demo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,108 @@
 "use strict";
+// https://www.scrapingbee.com/documentation/
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.doSomeStuff = doSomeStuff;
-console.log("Try npm run lint/fix!");
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-const trailing = 'Semicolon';
-const why = { am: 'I tabbed?' };
-const iWish = "I didn't have a trailing space...";
-const sicilian = true;
-;
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
-const re = /foo   bar/;
-function doSomeStuff(withThis, andThat, andThose) {
-    //function on one line
-    if (!Boolean(andThose.length)) {
-        return false;
+// Note to self: remember that the scraping might be done in different locales and languages, so
+// don't rely too much on localized stuff like aria values
+const scrapingbee_1 = require("scrapingbee");
+const fs_1 = require("fs");
+// ScrapingBee API key (watchout for credits)
+const APIKey = 'D3HROBYAXKQQFHLI07P4LAX4Q4MEYP1UV4PO6QV6024BSIGCYL7MSUDK0F05T0UMOPN76L2MOUAL3K5G';
+// A simple Google form test page
+const TestPageURL = "https://docs.google.com/forms/d/e/1FAIpQLSez2X2gKbvrc7K2c70eKAZqCuPlm_REtKkQR7nYI-uQaXjd7w/viewform";
+// Initialize a fresh ScrappingBee's client
+const client = new scrapingbee_1.ScrapingBeeClient(APIKey);
+// Service layer
+class AppleService {
+    constructor(client, url) {
+        // RTFM
+        this._getAvailableApplesRules = {
+            query: "return a list of apple types and discount rates (as numbers)",
+            rules: JSON.stringify({
+                kind: {
+                    description: "the kind of apple",
+                    type: "list"
+                },
+                discount: {
+                    description: "the discount rate",
+                    type: "list"
+                },
+            })
+        };
+        this._url = url;
+        this._client = client;
     }
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
+    // Scrape the demo Google form and returns our model
+    getAvailable() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield this._client.get({
+                url: this._url,
+                params: {
+                    custom_google: true,
+                    'ai_query': this._getAvailableApplesRules.query,
+                    'ai_extract_rules': this._getAvailableApplesRules.rules,
+                }
+            });
+            return JSON.parse(new TextDecoder().decode(response.data));
+        });
+    }
+    // Take a screenshot of the webview. Say 'cheese'
+    takeScreenshot(path_1) {
+        return __awaiter(this, arguments, void 0, function* (path, window_width = 375) {
+            const response = yield client.get({
+                url: this._url,
+                params: {
+                    custom_google: true,
+                    screenshot: true,
+                    screenshot_full_page: true,
+                    window_width,
+                }
+            });
+            (0, fs_1.writeFileSync)(path, response.data);
+        });
+    }
+    // Submit some answers in the demo Google form
+    submitDemoOrder() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Maybe factor out stuff like 'js_scenario' in production like we did
+            // for 'ai_query' and 'ai_extract_rules'
+            yield client.get({
+                url: this._url,
+                params: {
+                    custom_google: true,
+                    block_resources: false,
+                    'js_scenario': {
+                        'instructions': [
+                            { 'click': '#i12' },
+                            { 'click': '[aria-label="4"][data-value="4"]' },
+                            { 'fill': ['[name*="_month"]', '08'] },
+                            { 'fill': ['[name*="_day"]', '17'] },
+                            { 'fill': ['[name*="_year"]', '2025'] },
+                            { 'click': '[aria-label="Submit"]' }
+                        ]
+                    }
+                },
+            });
+        });
+    }
 }
-// TODO: more examples
+const appleService = new AppleService(client, TestPageURL);
+appleService.getAvailable()
+    .then((inventory) => console.log(`Successfully retrieved apples: ${JSON.stringify(inventory)}`))
+    .catch((e) => console.log('A problem occurred while retrieving apples: ' + e.response.data));
+appleService.takeScreenshot('apple_form.png')
+    .then(() => console.log('Successfully retrieved a screenshot'))
+    .catch((e) => console.log('A problem occurred while screenshotting: ' + e.response.data));
+// submitDemoOrder raises an exception because we are leaving the page after the form submit action
+// You can safely ignore it in this simple PoC, but take care of stuff like that in production
+appleService.submitDemoOrder()
+    .then(() => console.log('Demo order submitted'))
+    .catch((_) => console.log('Demo order submitted (yeah, this is not really an error. TBD)'));
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,127 @@
-console.log("Try npm run lint/fix!");
+// https://www.scrapingbee.com/documentation/
 
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+// Note to self: remember that the scraping might be done in different locales and languages, so
+// don't rely too much on localized stuff like aria values
 
-const trailing = 'Semicolon'
+import { ScrapingBeeClient } from "scrapingbee";
+import { writeFileSync } from "fs";
 
-			const why={am:'I tabbed?'};
+// ScrapingBee API key (watchout for credits)
 
-const iWish = "I didn't have a trailing space..."; 
+const APIKey = 'D3HROBYAXKQQFHLI07P4LAX4Q4MEYP1UV4PO6QV6024BSIGCYL7MSUDK0F05T0UMOPN76L2MOUAL3K5G';
 
-const sicilian = true;;
+// A simple Google form test page
 
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
+const TestPageURL = "https://docs.google.com/forms/d/e/1FAIpQLSez2X2gKbvrc7K2c70eKAZqCuPlm_REtKkQR7nYI-uQaXjd7w/viewform";
 
-const re = /foo   bar/;
+// Initialize a fresh ScrappingBee's client
 
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!Boolean(andThose.length)) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
+const client = new ScrapingBeeClient(APIKey);
+
+// Our object model
+
+interface AppleModel {
+  kind: string[],
+  discount: string[]
 }
-// TODO: more examples
+
+// Service layer
+
+class AppleService {
+  private _url: string;
+  private _client: ScrapingBeeClient;
+
+  // RTFM
+
+  private _getAvailableApplesRules = {
+    query: "return a list of apple types and discount rates (as numbers)",
+    rules: JSON.stringify({
+      kind: {
+        description: "the kind of apple",
+        type: "list"
+      },
+      discount: {
+        description: "the discount rate",
+        type: "list"
+      },
+    })
+  };
+
+  constructor(client: ScrapingBeeClient, url: string) {
+    this._url = url;
+    this._client = client
+  }
+
+  // Scrape the demo Google form and returns our model
+
+  async getAvailable(): Promise<AppleModel> {
+    const response = await this._client.get({
+      url: this._url,
+      params: {
+        custom_google: true,
+        'ai_query': this._getAvailableApplesRules.query,
+        'ai_extract_rules': this._getAvailableApplesRules.rules,
+      }
+    })
+    return JSON.parse(new TextDecoder().decode(response.data));
+  }
+
+  // Take a screenshot of the webview. Say 'cheese'
+
+  async takeScreenshot(path: string, window_width: number = 375) {
+    const response = await client.get({
+      url: this._url,
+      params: {
+        custom_google: true,
+        screenshot: true,
+        screenshot_full_page: true,
+        window_width,
+      }
+    });
+
+    writeFileSync(path, response.data);
+  }
+
+  // Submit some answers in the demo Google form
+
+  async submitDemoOrder() {
+
+    // Maybe factor out stuff like 'js_scenario' in production like we did
+    // for 'ai_query' and 'ai_extract_rules'
+
+    await client.get({
+      url: this._url,
+      params: {
+        custom_google: true,
+        block_resources: false,
+        'js_scenario': {
+          'instructions': [
+            { 'click': '#i12' },
+            { 'click': '[aria-label="4"][data-value="4"]' },
+            { 'fill': ['[name*="_month"]', '08'] },
+            { 'fill': ['[name*="_day"]', '17'] },
+            { 'fill': ['[name*="_year"]', '2025'] },
+            { 'click': '[aria-label="Submit"]' }
+          ]
+        }
+      },
+    })
+  }
+}
+
+const appleService = new AppleService(client, TestPageURL);
+
+appleService.getAvailable()
+  .then((inventory) => console.log(`Successfully retrieved apples: ${JSON.stringify(inventory)}`))
+  .catch((e) => console.log('A problem occurred while retrieving apples: ' + e.response.data));
+
+appleService.takeScreenshot('apple_form.png')
+  .then(() => console.log('Successfully retrieved a screenshot'))
+  .catch((e) => console.log('A problem occurred while screenshotting: ' + e.response.data));
+
+// submitDemoOrder raises an exception because we are leaving the page after the form submit action
+// You can safely ignore it in this simple PoC, but take care of stuff like that in production
+
+appleService.submitDemoOrder()
+  .then(() => console.log('Demo order submitted'))
+  .catch((_) => console.log('Demo order submitted (yeah, this is not really an error. See comments)'));
